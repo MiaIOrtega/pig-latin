50cde4ba5224b2ac0521f84cba9b687e234d22be
MiaIOrtega <miaortega2025@gmail.com>
2024-10-18 16:13:24 -0500

    Add files via upload

--- pig.java
+++ pig.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class pig
 {
     public static void main (String []arg){
-    //  Scanner input = new Scanner (System.in);
-    //   System.out.print("enter your fav expression");
-    //   String saying =input.nextLine();
-    //  int spaceNum=0;
-    //  int startNum=0;
+        //  Scanner input = new Scanner (System.in);
+        //   System.out.print("enter your fav expression");
+        //   String saying =input.nextLine();
+        //  int spaceNum=0;
+        //  int startNum=0;
         
         //for(int i=0; i<saying.length();i++){
         //   if (saying.charAt(i)==' '){
@@ -18,74 +18,41 @@ public class pig
         //   System.out.println(saying.substring(startNum));
         
         
-    //String word = new String ("mia");
-    //System.out.println(word);
-    //word=word.concat("m");
-    //word=word.substring(1);
-    //System.out.println(word);
-    //word=word.concat("ay");
-    //System.out.println(word);
+        //String word = new String ("mia");
+        //System.out.println(word);
+        //word=word.concat("m");
+        //word=word.substring(1);
+        //System.out.println(word);
+        //word=word.concat("ay");
+        //System.out.println(word);
     
-    Scanner input = new Scanner (System.in);    
-    System.out.print("enter your fav expression");
-    String saying =input.nextLine();
-    int spaceNum=0;
-    int startNum=0;
-    String add = " ";
-    String letter= " ";
+        Scanner input = new Scanner (System.in);    
+        System.out.print("enter your fav expression");
+        String saying = input.nextLine();
+        saying = saying.toLowerCase();
+        String p = new String ();
+        String temp = new String();
+    
+        int spaceNum=0;
+        int startNum=0;
         int i = 0;
         int num= 0;
-     for(int ii=0; i<saying.length();i++){
-           if (saying.charAt(ii)==' '){
-              num= saying.length ();
-              if (saying.charAt(ii)=='a') {
-            add= saying.substring(0,i);
-            saying=saying.concat("ay");
-            System.out.println(saying.substring(startNum,num));    
-            
-         }
-         else if (saying.charAt(ii)=='e')
-         {
-             add= saying.substring(0,i);
-            saying=saying.concat("ay");
-            System.out.println(saying.substring(startNum,num));    
-            
-            }
-             System.out.println(saying.substring(startNum,ii));    
-                    }
-        
-                }
-       // if (saying.charAt(i)=='a') {
-          //  add= saying.substring(0,i);
-         //   saying=saying.concat("ay");
-           // System.out.println(saying.substring(startNum,i));      
-        // }
-       //  else if (saying.charAt(i)=='e')
-        // {
-         //    add= saying.substring(0,i);
-         //   saying=saying.concat("ay");
-         //   System.out.println(saying.substring(startNum,i));    
-            
-          //  }
-         //   else if (saying.charAt(i)=='i')
-         //{
-             
-         //   saying=saying.concat("ay");
-         //   System.out.println(saying.substring(startNum,i));    
-            
-          //  }
-          //  else if (saying.charAt(i)=='o')
-        // {
-             
-          //  saying=saying.concat("ay");
-         //   System.out.println(saying.substring(startNum,i));    
-            
-           // }
-          //  else if (saying.charAt(i)=='u')
-         //{
-             
-          //  saying=saying.concat("ay");
-          //  System.out.println(saying.substring(startNum,i));       
+         for(int ii=0; ii<saying.length();ii++){
+               if (saying.charAt(ii)==' '){
+                       temp = saying.substring(startNum, ii);
+                       startNum = i+1; 
+                       if (temp.charAt(0) == 'a' || temp.charAt(0) == 'e' || temp.charAt(0) == 'i' || temp.charAt(0) == 'o' || temp.charAt(0) == 'u') {
+                              temp = temp.concat("yay"); 
+                              p= p.concat(temp);
+                       } else {
+                       
+                               
+                       
+                        }
+               }
+        }
+        System.out.println(p);
+           
          }
     }
 
